fea8f7a6124a533b06a3cb24e53c38a082abea1d
Draggor <draggor@touchlair.(none)>
2010-01-25 00:45:14 -0600

    Fixing Factories.java

--- src/main/java/net/dracolair/games/applestoapples/Factories.java
+++ src/main/java/net/dracolair/games/applestoapples/Factories.java
@@ -1,6 +1,8 @@
 package net.dracolair.games.applestoapples;
 
-public class Factories {
+public final class Factories {
+	
+	private Factories() {}
 	
 	public static Card CARD(String name, String desc) {
 		return new Card(name, desc);
